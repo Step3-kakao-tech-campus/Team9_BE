a4046cb2fb2f92cbdb46c1cfd5b6ed88694cd8ef
Taeho Kim <39511892+Train0303@users.noreply.github.com>
2023-09-15 17:17:51 +0900

    Conf: Initial Project (#9)

---
+++ linknamu/src/test/java/com/kakao/linknamu/LinknamuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.linknamu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinknamuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
