803fa697d7b8657e2387e3fb9dba6d0343ec4b30
Taeho <rjsdnxogh55@gmail.com>
2023-09-15 17:15:44 +0900

    Conf: Initial Project

---
+++ linknamu/src/main/java/com/kakao/linknamu/LinknamuApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.linknamu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinknamuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinknamuApplication.class, args);
+    }
+
+}
