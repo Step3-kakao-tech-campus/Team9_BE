ee8dcea72f451e66225e0a0ea45a94c707e860ae
Jaesung Son <77036161+JS970@users.noreply.github.com>
2023-09-19 13:56:38 +0900

    feat: 북마크 엔티티 구현
    ERD를 참고하여 북마크 엔티티를 구현했음

---
+++ linknamu/src/main/java/com/kakao/linknamu/Bookmark/Bookmark.java
@@ -0,0 +1,46 @@
+package com.kakao.linknamu.Bookmark;
+
+import com.kakao.linknamu.Category.Category;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name="bookmark_tb")
+public class Bookmark {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "bookmark_id")
+    private Long bookmark_id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(length = 100, nullable = false, unique = true, name = "bookmark_name")
+    private String bookmark_name;
+
+    @Column(length = 1024, nullable = false, name = "bookmark_link")
+    private String bookmark_link;
+
+    @Column(length = 200, name = "bookmark_description")
+    private String bookmark_description;
+
+    @Column(length = 512, name = "bookmark_thumbnail")
+    private String bookmark_thumbnail;
+
+
+    @Builder
+    public Bookmark(Long bookmark_id, Category category, String bookmark_name, String bookmark_link, String bookmark_description, String bookmark_thumbnail) {
+        this.bookmark_id = bookmark_id;
+        this.category = category;
+        this.bookmark_name = bookmark_name;
+        this.bookmark_link = bookmark_link;
+        this.bookmark_description = bookmark_description;
+        this.bookmark_thumbnail = bookmark_thumbnail;
+    }
+}
