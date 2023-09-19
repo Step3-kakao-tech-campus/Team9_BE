6c6b0fe3f9040ee5ee96d072ceef26c4c0846e32
Jaesung Son <77036161+JS970@users.noreply.github.com>
2023-09-19 14:02:10 +0900

    feat: 테그 엔티티 구현
    ERD를 참고하여 테그 엔티티를 구현했음

---
+++ linknamu/src/main/java/com/kakao/linknamu/Tag/Tag.java
@@ -0,0 +1,34 @@
+package com.kakao.linknamu.Tag;
+
+import com.kakao.linknamu.User.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name="tag_tb")
+public class Tag {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "tag_id")
+    private Long tag_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user_id;
+
+    @Column(length = 50, nullable = false, unique = true, name = "tag_name")
+    private String tag_name;
+
+
+    @Builder
+    public Tag(Long tag_id, User user, String tag_name) {
+        this.tag_id = tag_id;
+        this.user_id = user;
+        this.tag_name = tag_name;
+    }
+}
