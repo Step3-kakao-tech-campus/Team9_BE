87bafca80ad8a0d26489953b0b5d75303f1e2367
Jaesung Son <77036161+JS970@users.noreply.github.com>
2023-09-19 13:58:01 +0900

    feat: 카테고리 엔티티 구현
    ERD를 참고하여 카테고리 엔티티를 구현했음

---
+++ linknamu/src/main/java/com/kakao/linknamu/Category/Category.java
@@ -0,0 +1,39 @@
+package com.kakao.linknamu.Category;
+
+import com.kakao.linknamu.User.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name="category_tb")
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long category_id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category parent_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user_id;
+
+    @Column(length = 100, nullable = false, name = "category_name")
+    private String category_name;
+
+
+    @Builder
+    public Category(Long category_id, Category category, User user, String category_name) {
+        this.category_id = category_id;
+        this.parent_id = category;
+        this.user_id = user;
+        this.category_name = category_name;
+    }
+}
