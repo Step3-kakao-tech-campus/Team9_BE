208211ac68ef502f3e8d8c483b218338cb37a61e
Train0303 <rjsdnxogh55@gmail.com>
2023-09-24 23:28:10 +0900

    #18 Fix: FIx JwtExceptionFilter Exception Response
    - JwtExceptionFilter에서 예외 처리시 응답을 응답 컨벤션 형태에 맞게 수정했습니다.

--- linknamu/src/main/java/com/kakao/linknamu/_core/security/JwtExceptionFilter.java
+++ linknamu/src/main/java/com/kakao/linknamu/_core/security/JwtExceptionFilter.java
@@ -3,10 +3,15 @@ package com.kakao.linknamu._core.security;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kakao.linknamu._core.exception.Exception401;
+import com.kakao.linknamu._core.util.ApiUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,6 +21,8 @@ import java.io.IOException;
 // jwt 토큰관련 예외처리를 위한 클래스
 @Component
 public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    private static final ObjectMapper om = new ObjectMapper();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -28,9 +35,7 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
 
     private void setJwtExceptionResponse(HttpServletRequest request, HttpServletResponse response, Throwable exception) throws IOException {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        ObjectMapper om = new ObjectMapper();
-        response.getOutputStream().write(om.writeValueAsBytes(exception.getMessage()));
-        String responseBody = om.writeValueAsString(exception.getMessage());
-        response.getWriter().println(responseBody);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getOutputStream().write(om.writeValueAsBytes(ApiUtils.error(exception.getMessage(), HttpStatus.UNAUTHORIZED.value())));
     }
 }
