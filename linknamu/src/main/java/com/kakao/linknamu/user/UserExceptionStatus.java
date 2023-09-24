ec1c168b9a67b5e1e0b3c37d682473f3580ec918
Train0303 <rjsdnxogh55@gmail.com>
2023-09-24 22:22:44 +0900

    #17 feat: Add UserExceptionStatus
    - 구글 로그인 시 발생하는 예외를 추가했습니다.

--- linknamu/src/main/java/com/kakao/linknamu/user/UserExceptionStatus.java
+++ linknamu/src/main/java/com/kakao/linknamu/user/UserExceptionStatus.java
@@ -7,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum UserExceptionStatus implements BaseExceptionStatus {
-    USER_NOT_FOUND("회원이 존재하지 않습니다", 404);
-
+    USER_NOT_FOUND("회원이 존재하지 않습니다", 404),
+    GOOGLE_TOKEN_MISSING("구글 accessToken을 입력해주세요.", 401),
+    GOOGLE_TOKEN_INVALID("유효하지 않은 구글 accessToken입니다.", 400),
+    GOOGLE_API_CONNECTION_ERROR("구글 API 연동 중 문제가 발생했습니다", 500);
 
     @Getter
     private final String message;
