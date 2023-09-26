df04f4413f3c703ac6220400372acde8ce12863a
JS970 <son73097@pusan.ac.kr>
2023-09-26 19:32:47 +0900

    mod: bookmark패키지의 하위 패키지로 entity패키지를 생성함에 따른 경로 수정
    - controller, dto, entity, repository, service로 패키지 계층을 나누었고, 그에 따른 경로 수정 커밋임.

---
+++ linknamu/src/main/java/com/kakao/linknamu/bookmark/entity/Bookmark.java
@@ -0,0 +1,72 @@
+package com.kakao.linknamu.bookmark.entity;
+
+import com.kakao.linknamu.category.Category;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(
+        name="bookmark_tb",
+        uniqueConstraints={
+                @UniqueConstraint(
+                        name = "categoryId_bookmarkLink unique constraint",
+                        columnNames = {
+                                "category_id",
+                                "bookmark_link"
+                        }
+                )
+        }
+)
+public class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookmark_id")
+    private Long bookmarkId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(length = 100, nullable = false, name = "bookmark_name")
+    private String bookmarkName;
+
+    @Column(length = 1024, nullable = false, name = "bookmark_link")
+    private String bookmarkLink;
+
+    @Column(length = 200, name = "bookmark_description")
+    private String bookmarkDescription;
+
+    @Column(length = 512, name = "bookmark_thumbnail")
+    private String bookmarkThumbnail;
+
+
+    @Builder
+    public Bookmark(Long bookmarkId, Category category, String bookmarkName, String bookmarkLink, String bookmarkDescription, String bookmarkThumbnail) {
+        this.bookmarkId = bookmarkId;
+        this.category = category;
+        this.bookmarkName = bookmarkName;
+        this.bookmarkLink = bookmarkLink;
+        this.bookmarkDescription = bookmarkDescription;
+        this.bookmarkThumbnail = bookmarkThumbnail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bookmark bookmark = (Bookmark) o;
+        return Objects.equals(bookmarkId, bookmark.bookmarkId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookmarkId);
+    }
+}
