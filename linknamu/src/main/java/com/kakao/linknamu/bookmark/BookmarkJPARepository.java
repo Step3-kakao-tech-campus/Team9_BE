493bcddc331aef9e84589e3cea44e29d4e470e9b
JS970 <son73097@pusan.ac.kr>
2023-09-26 19:39:36 +0900

    del: Deprecated Files Deleted

--- linknamu/src/main/java/com/kakao/linknamu/bookmark/BookmarkJPARepository.java
+++
@@ -1,8 +0,0 @@
-package com.kakao.linknamu.bookmark;
-
-import com.kakao.linknamu.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookmarkJPARepository extends JpaRepository<User, Long> {
-
-}
