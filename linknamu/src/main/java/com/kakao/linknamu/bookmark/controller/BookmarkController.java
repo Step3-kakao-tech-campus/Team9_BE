b5948113ef02436dd59a24ec3e15c4931f6c0474
JS970 <son73097@pusan.ac.kr>
2023-09-26 19:28:16 +0900

    feat: Bookmark Controlller 생성, /api/bookmark/create 구현
    - Bookmark Create API

---
+++ linknamu/src/main/java/com/kakao/linknamu/bookmark/controller/BookmarkController.java
@@ -0,0 +1,22 @@
+package com.kakao.linknamu.bookmark.controller;
+
+import com.kakao.linknamu.bookmark.dto.BookmarkRequestDTO;
+import com.kakao.linknamu.bookmark.service.BookmarkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+//@RequestMapping("/api/bookmark")
+public class BookmarkController {
+    private final BookmarkService bookmarkService;
+
+    @PostMapping("bookmark/create")
+    public ResponseEntity<?> bookmarkCreate(BookmarkRequestDTO.bookmarkAddDTO dto, Error error){
+        bookmarkService.bookmarkAdd(dto);
+        return ResponseEntity.ok(null);
+    }
+}
