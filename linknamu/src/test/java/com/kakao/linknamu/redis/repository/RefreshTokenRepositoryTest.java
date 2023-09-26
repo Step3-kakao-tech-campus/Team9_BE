37cf244e359c759ce33b4bec6c09fe0719c22283
Taeho <rjsdnxogh55@gmail.com>
2023-09-27 01:34:18 +0900

    #29 Feat: RefreshToken 레포지토리 테스트 구현

---
+++ linknamu/src/test/java/com/kakao/linknamu/redis/repository/RefreshTokenRepositoryTest.java
@@ -0,0 +1,108 @@
+package com.kakao.linknamu.redis.repository;
+
+import com.kakao.linknamu._core.AbstractIntegrationContainerBaseTest;
+import com.kakao.linknamu._core.redis.entity.RefreshToken;
+import com.kakao.linknamu._core.redis.repository.RefreshTokenRepository;
+import com.kakao.linknamu._core.security.JwtProvider;
+import com.kakao.linknamu.user.entity.User;
+import com.kakao.linknamu.user.entity.constant.Provider;
+import com.kakao.linknamu.user.entity.constant.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RefreshTokenRepositoryTest extends AbstractIntegrationContainerBaseTest {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Autowired
+    public RefreshTokenRepositoryTest(RefreshTokenRepository refreshTokenRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    @DisplayName("Refresh 토큰으로 레디스 데이터를 조회할 수 있다")
+    @Test
+    void findByIdTest() {
+        //given
+        User user = User.builder().userId(1L)
+                .email("rjsdnxogh@naver.com")
+                .provider(Provider.PROVIDER_GOOGLE)
+                .role(Role.ROLE_USER)
+                .build();
+        String accessToken = JwtProvider.create(user);
+        String refreshToken = JwtProvider.createRefreshToken(user);
+        RefreshToken refreshTokenEntity = RefreshToken.of(refreshToken, accessToken, user);
+
+        refreshTokenRepository.save(refreshTokenEntity);
+
+        //when
+        RefreshToken findByrefreshToken = refreshTokenRepository.findById(refreshToken).get();
+
+        //then
+        assertEquals(refreshTokenEntity, findByrefreshToken);
+    }
+
+    @DisplayName("레디스에 Refresh 토큰이 없다면 null을 반환한다")
+    @Test
+    void findByIdNullTest() {
+        //given
+        User user = User.builder().userId(1L)
+                .email("rjsdnxogh@naver.com")
+                .provider(Provider.PROVIDER_GOOGLE)
+                .role(Role.ROLE_USER)
+                .build();
+        String accessToken = JwtProvider.create(user);
+        String refreshToken = JwtProvider.createRefreshToken(user);
+
+        //when
+        Optional<RefreshToken> findByrefreshToken = refreshTokenRepository.findById(refreshToken);
+
+        //then
+        assertTrue(findByrefreshToken.isEmpty());
+    }
+
+    @DisplayName("Access 토큰으로 레디스 데이터를 조회할 수 있다")
+    @Test
+    void findByAccessTokenTest() {
+        //given
+        User user = User.builder().userId(1L)
+                .email("rjsdnxogh@naver.com")
+                .provider(Provider.PROVIDER_GOOGLE)
+                .role(Role.ROLE_USER)
+                .build();
+        String accessToken = JwtProvider.create(user);
+        String refreshToken = JwtProvider.createRefreshToken(user);
+        RefreshToken refreshTokenEntity = RefreshToken.of(refreshToken, accessToken, user);
+
+        refreshTokenRepository.save(refreshTokenEntity);
+
+        //when
+        RefreshToken findByrefreshToken = refreshTokenRepository.findByAccessToken(accessToken).get();
+
+        //then
+        assertEquals(refreshTokenEntity, findByrefreshToken);
+    }
+
+    @DisplayName("레디스에 Access 토큰이 없다면 null을 반환한다")
+    @Test
+    void findByAccessTokenNullTest() {
+        //given
+        User user = User.builder().userId(1L)
+                .email("rjsdnxogh@naver.com")
+                .provider(Provider.PROVIDER_GOOGLE)
+                .role(Role.ROLE_USER)
+                .build();
+        String accessToken = JwtProvider.create(user);
+        String refreshToken = JwtProvider.createRefreshToken(user);
+
+        //when
+        Optional<RefreshToken> findByrefreshToken = refreshTokenRepository.findByAccessToken(accessToken);
+
+        //then
+        assertTrue(findByrefreshToken.isEmpty());
+    }
+}
