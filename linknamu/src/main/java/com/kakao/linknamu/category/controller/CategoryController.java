7d2a0fc0a384ed046977d15f159daac433b37940
ikyeong <grindabff@pusan.ac.kr>
2023-09-27 12:12:11 +0900

    #24 Feat: CategorySaveService 구현 및 Controller 수정

--- linknamu/src/main/java/com/kakao/linknamu/category/controller/CategoryController.java
+++ linknamu/src/main/java/com/kakao/linknamu/category/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.kakao.linknamu.category.controller;
 import com.kakao.linknamu._core.security.CustomUserDetails;
 import com.kakao.linknamu._core.util.ApiUtils;
 import com.kakao.linknamu.category.dto.CategorySaveRequestDto;
+import com.kakao.linknamu.category.service.CategorySaveService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/category")
 public class CategoryController {
 
+    private final CategorySaveService categorySaveService;
+
     @PostMapping("/create")
     public ResponseEntity<?> createCategory(
             @RequestBody @Valid CategorySaveRequestDto requestDto,
             Errors errors,
             @AuthenticationPrincipal CustomUserDetails user){
 
+        categorySaveService.save(requestDto, user.getUser());
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 }
---
+++ linknamu/src/main/java/com/kakao/linknamu/category/service/CategorySaveService.java
@@ -0,0 +1,20 @@
+package com.kakao.linknamu.category.service;
+
+import com.kakao.linknamu.category.dto.CategorySaveRequestDto;
+import com.kakao.linknamu.category.entity.Category;
+import com.kakao.linknamu.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategorySaveService {
+
+    private final CategoryService categoryService;
+
+    public void save(CategorySaveRequestDto requestDto, User user) {
+        Category parentCategory = categoryService.findByParentCategoryId(requestDto.parentCategoryId());
+        categoryService.save(requestDto.categoryName(), parentCategory, user);
+    }
+
+}
