5f6f454777a9191427660025cc4211adaea41356
JS970 <son73097@pusan.ac.kr>
2023-09-27 12:37:52 +0900

    - @Requestbody, @Valid 어노테이션 빠트린 것 추가했습니다.

--- linknamu/src/main/java/com/kakao/linknamu/bookmark/controller/BookmarkController.java
+++ linknamu/src/main/java/com/kakao/linknamu/bookmark/controller/BookmarkController.java
@@ -1,11 +1,13 @@
 package com.kakao.linknamu.bookmark.controller;
 
+import com.kakao.linknamu._core.util.ApiUtils;
 import com.kakao.linknamu.bookmark.dto.BookmarkRequestDTO;
 import com.kakao.linknamu.bookmark.service.BookmarkService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -15,8 +17,8 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @PostMapping("bookmark/create")
-    public ResponseEntity<?> bookmarkCreate(BookmarkRequestDTO.bookmarkAddDTO dto, Error error){
+    public ResponseEntity<?> bookmarkCreate(@RequestBody @Valid BookmarkRequestDTO.bookmarkAddDTO dto, Error error){
         bookmarkService.bookmarkAdd(dto);
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(ApiUtils.success(null));
     }
 }
