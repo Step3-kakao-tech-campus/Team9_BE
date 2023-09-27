2d97eb9f1927dd1c30f8aabe549b9ea121281f8d
ikyeong <grindabff@pusan.ac.kr>
2023-09-27 12:08:05 +0900

    #24 Feat: CategoryService 구현 및 예외클래스 생성

--- linknamu/src/main/java/com/kakao/linknamu/category/repository/CategoryJPARepository.java
+++ linknamu/src/main/java/com/kakao/linknamu/category/repository/CategoryJPARepository.java
@@ -2,6 +2,12 @@ package com.kakao.linknamu.category.repository;
 
 import com.kakao.linknamu.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CategoryJPARepository extends JpaRepository<Category, Long> {
+
+    @Query("select c from Category c where c.parentCategory.categoryId = :parentCategoryId")
+    Optional<Category> findByParentCategoryId(Long parentCategoryId);
 }
--- linknamu/src/main/java/com/kakao/linknamu/category/service/CategoryService.java
+++ linknamu/src/main/java/com/kakao/linknamu/category/service/CategoryService.java
@@ -1,10 +1,34 @@
 package com.kakao.linknamu.category.service;
 
+import com.kakao.linknamu._core.exception.Exception404;
+import com.kakao.linknamu.category.CategoryExceptionStatus;
+import com.kakao.linknamu.category.entity.Category;
+import com.kakao.linknamu.category.repository.CategoryJPARepository;
+import com.kakao.linknamu.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryJPARepository categoryJPARepository;
+
+    public void save(String categoryName, Category parentCategory, User user){
+        Category category = Category.builder()
+                .categoryName(categoryName)
+                .parentCategory(parentCategory)
+                .user(user)
+                .build();
+        categoryJPARepository.save(category);
+    }
+
+    public Category findByParentCategoryId(Long parentCategoryId) {
+        Category category = categoryJPARepository.findByParentCategoryId(parentCategoryId).orElseThrow(
+                () -> new Exception404(CategoryExceptionStatus.CATEGORY_NOT_FOUND)
+        );
+        return category;
+    }
+
 
 }
