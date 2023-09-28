1f2cd8a15e939ed0586ba787be6b53489f4646a4
ikyeong <grindabff@pusan.ac.kr>
2023-09-28 14:04:13 +0900

    #25 Feat: DTO of 메서드 구현

--- linknamu/src/main/java/com/kakao/linknamu/category/dto/PageInfoDto.java
+++ linknamu/src/main/java/com/kakao/linknamu/category/dto/PageInfoDto.java
@@ -1,9 +1,25 @@
 package com.kakao.linknamu.category.dto;
 
+import lombok.Builder;
+import org.springframework.data.domain.Pageable;
+
 public record PageInfoDto(
-        int totalCount,
+        long totalCount,
         int size,
         int currentPage,
         int totalPages
 ) {
+
+    @Builder
+    public PageInfoDto {
+    }
+
+    public static PageInfoDto of(Pageable pageable, long totalCount, int totalPages){
+        return PageInfoDto.builder()
+                .totalCount(totalCount)
+                .size(pageable.getPageSize())
+                .currentPage(pageable.getPageNumber())
+                .totalPages(totalPages)
+                .build();
+    }
 }
