54622bf80084d88f8a74384db298f4dc7af860b3
Train0303 <rjsdnxogh55@gmail.com>
2023-09-28 12:16:56 +0900

    #29 Fix: ActiveProfiles 추가
    - 기존 테스트에 환경설정을 지정하는 코드를 추가했습니다.

--- linknamu/src/test/java/com/kakao/linknamu/category/CategoryJPARepositoryTest.java
+++ linknamu/src/test/java/com/kakao/linknamu/category/CategoryJPARepositoryTest.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 public class CategoryJPARepositoryTest {
 
