7f0e78916bed185b8280881881ae9c4a97f5bc8f
JS970 <son73097@pusan.ac.kr>
2023-10-02 13:11:00 +0900

    \#54 Test: 북마크 API관련 Repository Test 코드 작성

--- linknamu/src/test/java/com/kakao/linknamu/bookmark/repository/BookmarkJPARepositoryTest.java
+++ linknamu/src/test/java/com/kakao/linknamu/bookmark/repository/BookmarkJPARepositoryTest.java
@@ -1,7 +1,6 @@
-package com.kakao.linknamu.bookmark;
+package com.kakao.linknamu.bookmark.repository;
 
 import com.kakao.linknamu.bookmark.entity.Bookmark;
-import com.kakao.linknamu.bookmark.repository.BookmarkJPARepository;
 import com.kakao.linknamu.category.entity.Category;
 import com.kakao.linknamu.category.repository.CategoryJPARepository;
 import com.kakao.linknamu.user.entity.User;
@@ -11,7 +10,6 @@ import com.kakao.linknamu.user.repository.UserJPARepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -60,12 +58,48 @@ public class BookmarkJPARepositoryTest {
         Bookmark savedBookmark = bookmarkJPARepository.save(bookmark);
 
         // then
-        assertThat(bookmark).isSameAs(savedBookmark);
+        assertThat(bookmark).isEqualTo(savedBookmark);
         assertThat(bookmark.getBookmarkId()).isSameAs(savedBookmark.getBookmarkId());
-        assertThat(bookmark.getCategory().getCategoryId()).isSameAs(savedBookmark.getCategory().getCategoryId());
-        assertThat(bookmark.getBookmarkName()).isSameAs(savedBookmark.getBookmarkName());
-        assertThat(bookmark.getBookmarkDescription()).isSameAs(savedBookmark.getBookmarkDescription());
-        assertThat(bookmark.getBookmarkLink()).isSameAs(savedBookmark.getBookmarkLink());
-        assertThat(bookmark.getBookmarkThumbnail()).isSameAs(savedBookmark.getBookmarkThumbnail());
+        assertThat(bookmark.getCategory().getCategoryId()).isEqualTo(savedBookmark.getCategory().getCategoryId());
+        assertThat(bookmark.getBookmarkName()).isEqualTo(savedBookmark.getBookmarkName());
+        assertThat(bookmark.getBookmarkDescription()).isEqualTo(savedBookmark.getBookmarkDescription());
+        assertThat(bookmark.getBookmarkLink()).isEqualTo(savedBookmark.getBookmarkLink());
+        assertThat(bookmark.getBookmarkThumbnail()).isEqualTo(savedBookmark.getBookmarkThumbnail());
+    }
+
+    @Test
+    @DisplayName("북마크_조회_테스트")
+    public void BookmarkSearchTest() {
+        // given
+        User user = User.builder()
+                .email("testemail@pusan.ac.kr")
+                .password("testpassword")
+                .provider(Provider.PROVIDER_NORMAL)
+                .role(Role.ROLE_USER)
+                .build();
+        user = userJPARepository.save(user);
+
+        Category category = Category.builder()
+                .categoryName("Category")
+                .parentCategory(null)
+                .user(user)
+                .build();
+        category = categoryJPARepository.save(category);
+
+        Bookmark bookmark = Bookmark.builder()
+                .bookmarkName("testbookmark")
+                .bookmarkLink("https://testbookmark.com")
+                .bookmarkDescription("description of testbookmark")
+                .bookmarkThumbnail("Thumbnail of testbookmark")
+                .category(category)
+                .build();
+        bookmark = bookmarkJPARepository.save(bookmark);
+
+        // when
+        Bookmark findById = bookmarkJPARepository.findById(bookmark.getBookmarkId()).orElseThrow();
+
+        // then
+        assertThat(findById.getBookmarkLink()).isEqualTo(bookmark.getBookmarkLink());
+        assertThat(findById.getBookmarkName()).isEqualTo(bookmark.getBookmarkName());
     }
 }
