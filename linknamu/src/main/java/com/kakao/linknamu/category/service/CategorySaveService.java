895e582f316d91b3aab4fe7fd1ea497b4b3a50a6
ikyeong <grindabff@pusan.ac.kr>
2023-10-05 12:23:23 +0900

    #64 Feat: 카테고리 추가하기 예외처리 구현

--- linknamu/src/main/java/com/kakao/linknamu/category/service/CategorySaveService.java
+++ linknamu/src/main/java/com/kakao/linknamu/category/service/CategorySaveService.java
@@ -5,6 +5,7 @@ import com.kakao.linknamu._core.exception.Exception404;
 import com.kakao.linknamu.category.CategoryExceptionStatus;
 import com.kakao.linknamu.category.dto.CategorySaveRequestDto;
 import com.kakao.linknamu.user.entity.User;
+import com.kakao.linknamu.workspace.WorkspaceExceptionStatus;
 import com.kakao.linknamu.workspace.entity.Workspace;
 import com.kakao.linknamu.workspace.repository.WorkspaceJPARepository;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,11 @@ import org.springframework.stereotype.Service;
 public class CategorySaveService {
 
     private final CategoryService categoryService;
-    // 아직 워크스페이스 서비스에서 findById가 구현되어 있지 않아 레포지토리로 접근했습니다. 이후 수정하겠습니다.
     private final WorkspaceJPARepository workspaceJPARepository;
 
     public void save(CategorySaveRequestDto requestDto, User user) {
-        Workspace workspace = workspaceJPARepository.findById(requestDto.workspaceId()).orElseThrow(
-                // throw Exception404
+        Workspace workspace = workspaceJPARepository.findById(requestDto.workspaceId()).orElseThrow(() ->
+                new Exception404(WorkspaceExceptionStatus.WORKSPACE_NOT_FOUND)
         );
 
         categoryService.validUser(workspace, user);
