498e972ffa199700309f0ef427a7b9364f80f111
ikyeong <grindabff@pusan.ac.kr>
2023-10-08 20:21:05 +0900

    #93 Feat: 컨트롤러 생성

---
+++ linknamu/src/main/java/com/kakao/linknamu/bookmark/controller/BookmarkSearchController.java
@@ -0,0 +1,10 @@
+package com.kakao.linknamu.bookmark.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BookmarkSearchController {
+
+}
