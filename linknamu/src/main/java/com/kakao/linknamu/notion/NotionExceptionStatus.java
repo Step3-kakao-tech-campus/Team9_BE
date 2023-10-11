c151cff3a9fab8e79115320450b5be43868357e7
Taeho <rjsdnxogh55@gmail.com>
2023-10-11 21:12:30 +0900

    #84 Feat: 노션 연동 계정 삭제 API 구현

--- linknamu/src/main/java/com/kakao/linknamu/notion/NotionExceptionStatus.java
+++ linknamu/src/main/java/com/kakao/linknamu/notion/NotionExceptionStatus.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum NotionExceptionStatus implements BaseExceptionStatus {
-    NOTION_ALREADY_EXIST("이미 존재하는 데이터입니다.", 400);
+    NOTION_ALREADY_EXIST("이미 존재하는 데이터입니다.", 400),
+    NOTION_FORBIDDEN("권한이 없는 접근입니다.", 403),
+    NOTION_ACCOUNT_NOT_FOUND("존재하지 않는 데이터입니다.", 404);
 
     @Getter
     private final String message;
--- linknamu/src/main/java/com/kakao/linknamu/notion/controller/NotionController.java
+++ linknamu/src/main/java/com/kakao/linknamu/notion/controller/NotionController.java
@@ -4,14 +4,12 @@ import com.kakao.linknamu._core.security.CustomUserDetails;
 import com.kakao.linknamu._core.util.ApiUtils;
 import com.kakao.linknamu.notion.dto.NotionApiRegistrationRequestDto;
 import com.kakao.linknamu.notion.service.NotionApiCreateService;
+import com.kakao.linknamu.notion.service.NotionApiDeleteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,12 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotionController {
 
     private final NotionApiCreateService notionApiCreateService;
+    private final NotionApiDeleteService notionApiDeleteService;
 
     @PostMapping("/registration")
     public ResponseEntity<?> apiRegistration(@RequestBody @Valid NotionApiRegistrationRequestDto requestDto,
                                              @AuthenticationPrincipal CustomUserDetails userDetails) {
         // api 등록 로직
         notionApiCreateService.createNotionApi(requestDto, userDetails.getUser());
+        return ResponseEntity.ok(ApiUtils.success(null));
+    }
+
+    @PostMapping("/delete/{notion_account}")
+    public ResponseEntity<?> apiDelete(@PathVariable(name = "notion_account") Long notionAccountId,
+                                       @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        //api 등록 취소 로직
+        notionApiDeleteService.deleteNotionAccount(customUserDetails.getUser(), notionAccountId);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 }
