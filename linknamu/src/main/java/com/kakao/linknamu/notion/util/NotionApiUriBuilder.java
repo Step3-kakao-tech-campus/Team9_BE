566f88283207bd5738c1ce15f33fa4be3309a020
Taeho <rjsdnxogh55@gmail.com>
2023-10-12 01:18:01 +0900

    #84 Feat: 노션 API 배치 서비스 개발

---
+++ linknamu/src/main/java/com/kakao/linknamu/notion/util/NotionApiUriBuilder.java
@@ -0,0 +1,23 @@
+package com.kakao.linknamu.notion.util;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class NotionApiUriBuilder {
+    private static final String BLOCK_URI = "https://api.notion.com/v1/blocks/";
+    private static final int PAGE_SIZE = 100;
+
+    public String getBlockUri(String pageId, Optional<String> nextCursor) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BLOCK_URI)
+                .append(pageId)
+                .append("/children?")
+                .append(String.format("page_size=%d", PAGE_SIZE));
+
+        nextCursor.ifPresent(s -> sb.append(String.format("start_cursor=%s", s)));
+
+        return sb.toString();
+    }
+}
