7b948b3f070efe66897560cedc518732c9c2495b
JS970 <son73097@pusan.ac.kr>
2023-10-14 21:42:30 +0900

    #103 Feat: 존재 유무 확인을 위한 repo 메서드 선언
    - GooglePageJPARepository.existsByDocumentIdAndUser
    -

--- linknamu/src/main/java/com/kakao/linknamu/googleDocs/repository/GooglePageJPARepository.java
+++ linknamu/src/main/java/com/kakao/linknamu/googleDocs/repository/GooglePageJPARepository.java
@@ -1,8 +1,10 @@
 package com.kakao.linknamu.googleDocs.repository;
 
 import com.kakao.linknamu.googleDocs.entity.GooglePage;
+import com.kakao.linknamu.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GooglePageJPARepository extends JpaRepository<GooglePage, Long> {
-
+    boolean existsByDocumentIdAndUser(String documentId, User user);
 }
