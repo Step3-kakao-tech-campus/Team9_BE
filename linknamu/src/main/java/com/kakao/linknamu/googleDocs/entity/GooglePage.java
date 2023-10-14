8d7165428abb110c423a06066986b82a47225e3e
JS970 <son73097@pusan.ac.kr>
2023-10-14 17:08:24 +0900

    #103 Feat: GooglePage 엔티티 생성

---
+++ linknamu/src/main/java/com/kakao/linknamu/googleDocs/entity/GooglePage.java
@@ -0,0 +1,61 @@
+package com.kakao.linknamu.googleDocs.entity;
+
+import com.kakao.linknamu._core.util.AuditingEntity;
+import com.kakao.linknamu.category.entity.Category;
+import com.kakao.linknamu.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Objects;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class GooglePage extends AuditingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "document_id", nullable = false)
+    private String documentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Category category;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    @Builder
+    public GooglePage(Long id, User user, String documentId, Category category, Boolean isActive) {
+        this.id = id;
+        this.user = user;
+        this.documentId = documentId;
+        this.category = category;
+        this.isActive = isActive;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GooglePage that = (GooglePage) o;
+        return Objects.equals(id, that.id) && Objects.equals(documentId, that.documentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, documentId);
+    }
+}
