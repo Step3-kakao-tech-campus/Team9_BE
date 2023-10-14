81e4d52baaa408c063461c79d60d0a61b10f342c
JS970 <son73097@pusan.ac.kr>
2023-10-14 17:37:49 +0900

    #103 Feat: GooglePageJPARepository 생성

---
+++ linknamu/src/main/java/com/kakao/linknamu/googleDocs/repository/GooglePageJPARepository.java
@@ -0,0 +1,8 @@
+package com.kakao.linknamu.googleDocs.repository;
+
+import com.kakao.linknamu.googleDocs.entity.GooglePage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GooglePageJPARepository extends JpaRepository<GooglePage, Long> {
+
+}
