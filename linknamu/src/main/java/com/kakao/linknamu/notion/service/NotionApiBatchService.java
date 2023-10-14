288d2f75c0047357533672cb4122e261b5c380bd
Train0303 <rjsdnxogh55@gmail.com>
2023-10-14 21:47:43 +0900

    #105 Feat: 북마크, 임베드 링크 추출 구현

--- linknamu/src/main/java/com/kakao/linknamu/notion/service/NotionApiBatchService.java
+++ linknamu/src/main/java/com/kakao/linknamu/notion/service/NotionApiBatchService.java
@@ -10,12 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.yaml.snakeyaml.parser.ParserException;
@@ -76,24 +74,14 @@ public class NotionApiBatchService {
                 for (Object o : result) {
                     JSONObject subObject = (JSONObject) o;
                     String type = (String) subObject.get("type");
-                    JSONObject subObject2 = (JSONObject) subObject.get(type);
-                    JSONArray richTexts = (JSONArray) subObject2.get("rich_text");
-                    if (Objects.isNull(richTexts)) continue;
-
-                    for (Object richText : richTexts) {
-                        String href = (String) ((JSONObject) richText).get("href");
-                        String plainText = (String) ((JSONObject) richText).get("plain_text");
-                        if (Objects.nonNull(href)) {
-                            resultBookmarks.add(Bookmark.builder()
-                                            .bookmarkLink(href.startsWith("/") ? "https://www.notion.so" + href : href)
-                                            .bookmarkName(plainText.length() > 100 ? plainText.substring(0, 100) : plainText)
-                                            .category(notionPage.getCategory())
-                                            .build());
-                        }
+                    JSONObject subObjectChild = (JSONObject) subObject.get(type);
+                    if(type.equals("bookmark") | type.equals("embed")) {
+                        getBookmarkOrEmbedLink(subObjectChild, resultBookmarks, notionPage);
+                    } else {
+                        getOtherLink(subObjectChild, resultBookmarks, notionPage);
                     }
                 }
 
-//                Thread.sleep(1000); // -> 다음 호출이 필요할 땐 1초 쉬어준다.(멀티 쓰레드 환경에서의 Rate_limit 대비)
             } catch(HttpClientErrorException e) {
                 if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
                     log.error("유효하지 않은 토큰");
@@ -115,5 +103,45 @@ public class NotionApiBatchService {
 
 
         return resultBookmarks;
+    }
+
+    // 노션 링크 데이터 종류 중 북마크와 임베드 형태를 가져온다.
+    private void getBookmarkOrEmbedLink(JSONObject bookmarkTypeObject, List<Bookmark> resultBookmarks, NotionPage notionPage) {
+        JSONArray caption = (JSONArray) bookmarkTypeObject.get("caption");
+        String url = (String) bookmarkTypeObject.get("url");
+        String captionText = url;
+        if (!caption.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<caption.size(); i++) {
+                JSONObject captionObject = (JSONObject) caption.get(i);
+                sb.append(captionObject.get("plain_text"));
+                captionText = sb.toString().strip();
+            }
+        }
+
+        resultBookmarks.add(Bookmark.builder()
+                .bookmarkLink(url)
+                .bookmarkName(captionText.length() > 100 ? captionText.substring(0, 100) : captionText)
+                .category(notionPage.getCategory())
+                .build()
+        );
+    }
+
+    // 북마크, 임베드 이외의 링크 데이터를 가져온다.
+    private void getOtherLink(JSONObject otherTypeObject, List<Bookmark> resultBookmarks, NotionPage notionPage) {
+        JSONArray richTexts = (JSONArray) otherTypeObject.get("rich_text");
+        if (Objects.isNull(richTexts)) return;
+
+        for (Object richText : richTexts) {
+            String href = (String) ((JSONObject) richText).get("href");
+            String plainText = (String) ((JSONObject) richText).get("plain_text");
+            if (Objects.nonNull(href)) {
+                resultBookmarks.add(Bookmark.builder()
+                        .bookmarkLink(href.startsWith("/") ? "https://www.notion.so" + href : href)
+                        .bookmarkName(plainText.length() > 100 ? plainText.substring(0, 100) : plainText)
+                        .category(notionPage.getCategory())
+                        .build());
+            }
+        }
     }
 }
