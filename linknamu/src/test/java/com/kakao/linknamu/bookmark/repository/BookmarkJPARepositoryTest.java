533d0a23911f24c74da97ed3054984574a7a3c9d
ikyeong <grindabff@pusan.ac.kr>
2023-10-16 17:02:36 +0900

    #114 Mod: 주석 처리된 테스트 코드 수정

--- linknamu/src/test/java/com/kakao/linknamu/bookmark/repository/BookmarkJPARepositoryTest.java
+++ linknamu/src/test/java/com/kakao/linknamu/bookmark/repository/BookmarkJPARepositoryTest.java
@@ -32,81 +32,52 @@ public class BookmarkJPARepositoryTest {
     @Autowired
     WorkspaceJPARepository workspaceJPARepository;
 
-//    @Test
-//    @DisplayName("북마크_생성_테스트")
-//    public void BookmarkSaveTest() {
-//        // given
-//        User user = User.builder()
-//                .email("testemail@pusan.ac.kr")
-//                .password("testpassword")
-//                .provider(Provider.PROVIDER_NORMAL)
-//                .role(Role.ROLE_USER)
-//                .build();
-//        user = userJPARepository.save(user);
-//
-//        Category category = Category.builder()
-//                .categoryName("Category")
-//                .parentCategory(null)
-//                .user(user)
-//                .build();
-//        Category rootCategory = categoryJPARepository.save(category);
-//
-//        Bookmark bookmark = Bookmark.builder()
-//                .bookmarkName("testbookmark")
-//                .bookmarkLink("https://testbookmark.com")
-//                .bookmarkDescription("description of testbookmark")
-//                .bookmarkThumbnail("Thumbnail of testbookmark")
-//                .category(rootCategory)
-//                .build();
-//
-//        // when
-//        Bookmark savedBookmark = bookmarkJPARepository.save(bookmark);
-//
-//        // then
-//        assertThat(bookmark).isEqualTo(savedBookmark);
-//        assertThat(bookmark.getBookmarkId()).isSameAs(savedBookmark.getBookmarkId());
-//        assertThat(bookmark.getCategory().getCategoryId()).isEqualTo(savedBookmark.getCategory().getCategoryId());
-//        assertThat(bookmark.getBookmarkName()).isEqualTo(savedBookmark.getBookmarkName());
-//        assertThat(bookmark.getBookmarkDescription()).isEqualTo(savedBookmark.getBookmarkDescription());
-//        assertThat(bookmark.getBookmarkLink()).isEqualTo(savedBookmark.getBookmarkLink());
-//        assertThat(bookmark.getBookmarkThumbnail()).isEqualTo(savedBookmark.getBookmarkThumbnail());
-//    }
-//
-//    @Test
-//    @DisplayName("북마크_조회_테스트")
-//    public void BookmarkSearchTest() {
-//        // given
-//        User user = User.builder()
-//                .email("testemail@pusan.ac.kr")
-//                .password("testpassword")
-//                .provider(Provider.PROVIDER_NORMAL)
-//                .role(Role.ROLE_USER)
-//                .build();
-//        user = userJPARepository.save(user);
-//
-//        Category category = Category.builder()
-//                .categoryName("Category")
-//                .parentCategory(null)
-//                .user(user)
-//                .build();
-//        category = categoryJPARepository.save(category);
-//
-//        Bookmark bookmark = Bookmark.builder()
-//                .bookmarkName("testbookmark")
-//                .bookmarkLink("https://testbookmark.com")
-//                .bookmarkDescription("description of testbookmark")
-//                .bookmarkThumbnail("Thumbnail of testbookmark")
-//                .category(category)
-//                .build();
-//        bookmark = bookmarkJPARepository.save(bookmark);
-//
-//        // when
-//        Bookmark findById = bookmarkJPARepository.findById(bookmark.getBookmarkId()).orElseThrow();
-//
-//        // then
-//        assertThat(findById.getBookmarkLink()).isEqualTo(bookmark.getBookmarkLink());
-//        assertThat(findById.getBookmarkName()).isEqualTo(bookmark.getBookmarkName());
-//    }
+    @Test
+    @DisplayName("북마크_생성_테스트")
+    public void BookmarkSaveTest() {
+        // given
+        User user = User.builder()
+                .email("testemail@pusan.ac.kr")
+                .password("testpassword")
+                .provider(Provider.PROVIDER_NORMAL)
+                .role(Role.ROLE_USER)
+                .build();
+        user = userJPARepository.save(user);
+
+        Workspace workspace = Workspace.builder()
+                .workspaceName("테스트 워크스페이스")
+                .user(user)
+                .linkProvider(LinkProvider.NORMAL)
+                .build();
+        workspaceJPARepository.save(workspace);
+
+        Category category = Category.builder()
+                .categoryName("Category")
+                .workspace(workspace)
+                .build();
+        Category rootCategory = categoryJPARepository.save(category);
+
+        Bookmark bookmark = Bookmark.builder()
+                .bookmarkName("testbookmark")
+                .bookmarkLink("https://testbookmark.com")
+                .bookmarkDescription("description of testbookmark")
+                .bookmarkThumbnail("Thumbnail of testbookmark")
+                .category(rootCategory)
+                .build();
+
+        // when
+        Bookmark savedBookmark = bookmarkJPARepository.save(bookmark);
+
+        // then
+        assertThat(bookmark).isEqualTo(savedBookmark);
+        assertThat(bookmark.getBookmarkId()).isSameAs(savedBookmark.getBookmarkId());
+        assertThat(bookmark.getCategory().getCategoryId()).isEqualTo(savedBookmark.getCategory().getCategoryId());
+        assertThat(bookmark.getBookmarkName()).isEqualTo(savedBookmark.getBookmarkName());
+        assertThat(bookmark.getBookmarkDescription()).isEqualTo(savedBookmark.getBookmarkDescription());
+        assertThat(bookmark.getBookmarkLink()).isEqualTo(savedBookmark.getBookmarkLink());
+        assertThat(bookmark.getBookmarkThumbnail()).isEqualTo(savedBookmark.getBookmarkThumbnail());
+    }
+
 
     @Test
     @DisplayName("북마크_조회_조인_테스트")
--- linknamu/src/test/java/com/kakao/linknamu/bookmarkTag/repository/BookmarkTagJPARepositoryTest.java
+++ linknamu/src/test/java/com/kakao/linknamu/bookmarkTag/repository/BookmarkTagJPARepositoryTest.java
@@ -35,51 +35,49 @@ public class BookmarkTagJPARepositoryTest {
     @Autowired
     BookmarkTagJPARepository bookmarkTagJPARepository;
 
-//    @Test
-//    @DisplayName("북마크-태그_생성_테스트")
-//    public void BookmarkTagSaveTest() {
-//        // given
-//        User user = User.builder()
-//                .email("testemail@pusan.ac.kr")
-//                .password("testpassword")
-//                .provider(Provider.PROVIDER_NORMAL)
-//                .role(Role.ROLE_USER)
-//                .build();
-//        user = userJPARepository.save(user);
-//
-//        Category category = Category.builder()
-//                .categoryName("Category")
-//                .parentCategory(null)
-//                .user(user)
-//                .build();
-//        Category rootCategory = categoryJPARepository.save(category);
-//
-//        Bookmark bookmark = Bookmark.builder()
-//                .bookmarkName("testbookmark")
-//                .bookmarkLink("https://testbookmark.com")
-//                .bookmarkDescription("description of testbookmark")
-//                .bookmarkThumbnail("Thumbnail of testbookmark")
-//                .category(rootCategory)
-//                .build();
-//        bookmarkJPARepository.save(bookmark);
-//
-//        Tag tag = Tag.builder()
-//                .tagName("testTag")
-//                .user(user)
-//                .build();
-//        tagJPARepository.save(tag);
-//
-//        BookmarkTag bookmarkTag = BookmarkTag.builder()
-//                .bookmark(bookmark)
-//                .tag(tag)
-//                .build();
-//
-//        // when
-//        BookmarkTag savedBookmarkTag = bookmarkTagJPARepository.save(bookmarkTag);
-//
-//        // then
-//        assertThat(bookmarkTag).isEqualTo(savedBookmarkTag);
-//        assertThat(savedBookmarkTag.getBookmark().getBookmarkId()).isEqualTo(bookmark.getBookmarkId());
-//        assertThat(savedBookmarkTag.getTag().getTagId()).isEqualTo(tag.getTagId());
-//    }
+    @Test
+    @DisplayName("북마크-태그_생성_테스트")
+    public void BookmarkTagSaveTest() {
+        // given
+        User user = User.builder()
+                .email("testemail@pusan.ac.kr")
+                .password("testpassword")
+                .provider(Provider.PROVIDER_NORMAL)
+                .role(Role.ROLE_USER)
+                .build();
+        user = userJPARepository.save(user);
+
+        Category category = Category.builder()
+                .categoryName("Category")
+                .build();
+        Category rootCategory = categoryJPARepository.save(category);
+
+        Bookmark bookmark = Bookmark.builder()
+                .bookmarkName("testbookmark")
+                .bookmarkLink("https://testbookmark.com")
+                .bookmarkDescription("description of testbookmark")
+                .bookmarkThumbnail("Thumbnail of testbookmark")
+                .category(rootCategory)
+                .build();
+        bookmarkJPARepository.save(bookmark);
+
+        Tag tag = Tag.builder()
+                .tagName("testTag")
+                .user(user)
+                .build();
+        tagJPARepository.save(tag);
+
+        BookmarkTag bookmarkTag = BookmarkTag.builder()
+                .bookmark(bookmark)
+                .tag(tag)
+                .build();
+
+        // when
+        BookmarkTag savedBookmarkTag = bookmarkTagJPARepository.save(bookmarkTag);
+
+        // then
+        assertThat(bookmarkTag).isEqualTo(savedBookmarkTag);
+        assertThat(savedBookmarkTag.getBookmark().getBookmarkId()).isEqualTo(bookmark.getBookmarkId());
+        assertThat(savedBookmarkTag.getTag().getTagId()).isEqualTo(tag.getTagId());
+    }
 }
