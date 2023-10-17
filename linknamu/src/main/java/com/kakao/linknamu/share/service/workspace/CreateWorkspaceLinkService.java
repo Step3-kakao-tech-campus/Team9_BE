ad30ee28b482d7617da3a7595fb56c5e0753a978
YoungJin Cho <whdudwls0285@pusan.ac.kr>
2023-10-17 18:43:42 +0900

    Fix: 워크스페이스 링크생성시 해당 id의 워크스페이스 존재 확인

--- linknamu/src/main/java/com/kakao/linknamu/share/service/workspace/CreateWorkspaceLinkService.java
+++ linknamu/src/main/java/com/kakao/linknamu/share/service/workspace/CreateWorkspaceLinkService.java
@@ -2,6 +2,7 @@ package com.kakao.linknamu.share.service.workspace;
 
 import com.kakao.linknamu._core.encryption.AESEncryption;
 import com.kakao.linknamu.share.dto.workspace.CreateWorkSpaceLinkRequestDto;
+import com.kakao.linknamu.workspace.service.WorkspaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +14,10 @@ public class CreateWorkspaceLinkService {
 
 
     private final AESEncryption aesEncryption;
+    private final WorkspaceService workspaceService;
 
     public String createLink(CreateWorkSpaceLinkRequestDto requestDto) {
-
-
+        workspaceService.getWorkspaceById(requestDto.workSpaceId());
         String encodedString = aesEncryption.encode(requestDto.workSpaceId().toString());
 
 
