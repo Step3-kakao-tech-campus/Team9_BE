2fa37d63195a7018de362a325ffa09411cc9f0a7
Taeho <rjsdnxogh55@gmail.com>
2023-10-17 21:35:19 +0900

    #105 Style: 코드 자동정렬 실행

--- linknamu/src/main/java/com/kakao/linknamu/notion/service/NotionApiBatchService.java
+++ linknamu/src/main/java/com/kakao/linknamu/notion/service/NotionApiBatchService.java
@@ -58,13 +58,13 @@ public class NotionApiBatchService {
         List<Bookmark> resultBookmarks = new ArrayList<>();
         JSONParser jsonParser = jsonParserProvider.getObject();
 
-        while(hasMore) {
+        while (hasMore) {
             HttpHeaders headers = new HttpHeaders();
             headers.set("Authorization", accessToken);
             headers.set("Notion-Version", NOTION_VERSION);
             HttpEntity<?> httpEntity = new HttpEntity<>(headers);
             String uri = notionApiUriBuilder.getBlockUri(pageId, Optional.ofNullable(nextCursor));
-            try{
+            try {
                 ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, String.class);
                 JSONObject jsonObject = (JSONObject) jsonParser.parse(response.getBody());
                 JSONArray result = (JSONArray) jsonObject.get("results");
@@ -75,33 +75,32 @@ public class NotionApiBatchService {
                     JSONObject subObject = (JSONObject) o;
                     String type = (String) subObject.get("type");
                     JSONObject subObjectChild = (JSONObject) subObject.get(type);
-                    if(type.equals("bookmark") | type.equals("embed")) {
+                    if (type.equals("bookmark") | type.equals("embed")) {
                         getBookmarkOrEmbedLink(subObjectChild, resultBookmarks, notionPage);
                     } else {
                         getOtherLink(subObjectChild, resultBookmarks, notionPage);
                     }
                 }
 
-            } catch(HttpClientErrorException e) {
+            } catch (HttpClientErrorException e) {
                 if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
                     log.error("유효하지 않은 토큰");
                     throw new InvalidNotionApiException();
-                }else if(e.getStatusCode() == HttpStatus.TOO_MANY_REQUESTS) {
+                } else if (e.getStatusCode() == HttpStatus.TOO_MANY_REQUESTS) {
                     log.error("요청 한도 초과");
-                }else if(e.getStatusCode() == HttpStatus.NOT_FOUND){
+                } else if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
                     log.error("잘못된 Page Id");
                     throw new InvalidNotionApiException();
-                }else {
+                } else {
                     log.error(e.getMessage());
                 }
-            } catch(ParserException e) {
+            } catch (ParserException e) {
                 log.error("올바르지 않은 json parser : " + e.getMessage());
-            } catch(Exception e) {
+            } catch (Exception e) {
                 log.error(e.getMessage());
             }
         }
-
-
+        
         return resultBookmarks;
     }
 
@@ -112,7 +111,7 @@ public class NotionApiBatchService {
         String captionText = url;
         if (!caption.isEmpty()) {
             StringBuilder sb = new StringBuilder();
-            for(int i=0; i<caption.size(); i++) {
+            for (int i = 0; i < caption.size(); i++) {
                 JSONObject captionObject = (JSONObject) caption.get(i);
                 sb.append(captionObject.get("plain_text"));
                 captionText = sb.toString().strip();
