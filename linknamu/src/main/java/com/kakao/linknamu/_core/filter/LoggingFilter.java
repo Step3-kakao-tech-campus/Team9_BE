6bbced1b24d6fcf6e79ea21d67eb9226ea034d3a
ikyeong <grindabff@pusan.ac.kr>
2023-10-29 17:06:21 +0900

    #140 Feat: LoggingFilter 구현

---
+++ linknamu/src/main/java/com/kakao/linknamu/_core/filter/LoggingFilter.java
@@ -0,0 +1,81 @@
+package com.kakao.linknamu._core.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@WebFilter(urlPatterns = "/api/*")
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(httpRequest);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(httpResponse);
+
+        chain.doFilter(requestWrapper, responseWrapper);
+
+        log.info("\n" +
+                        "[REQUEST] {} - {} {}\n" +
+                        "Headers : {}\n" +
+                        "Request : {}\n" +
+                        "Response : {}\n",
+                ((HttpServletRequest) request).getMethod(),
+                ((HttpServletRequest) request).getRequestURI(),
+                responseWrapper.getStatus(),
+                getHeaders((HttpServletRequest) request),
+                getRequestBody(requestWrapper),
+                getResponseBody(responseWrapper));
+    }
+
+    private Map getHeaders(HttpServletRequest request) {
+        Map<String, Object> headerMap = new HashMap<>();
+
+        Enumeration<String> headerArray = request.getHeaderNames();
+        while (headerArray.hasMoreElements()) {
+            String headerName = headerArray.nextElement();
+            headerMap.put(headerName, request.getHeader(headerName));
+        }
+        return headerMap;
+    }
+
+    private String getRequestBody(ContentCachingRequestWrapper request) {
+        ContentCachingRequestWrapper wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                try {
+                    return new String(buf, 0, buf.length, wrapper.getCharacterEncoding());
+                } catch (UnsupportedEncodingException e) {
+                    return " - ";
+                }
+            }
+        }
+        return " - ";
+    }
+
+    private String getResponseBody(final HttpServletResponse response) throws IOException {
+        ContentCachingResponseWrapper wrapper =
+                WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                wrapper.copyBodyToResponse();
+                return new String(buf, 0, buf.length, wrapper.getCharacterEncoding());
+            }
+        }
+        return " - ";
+    }
+}
