1a8f8c31be2684cfc922b503df80496cc7e362fa
YoungJin Cho <whdudwls0285@pusan.ac.kr>
2023-11-03 21:07:34 +0900

    #157 Feat: 북마크 도메인 컨벤션 완료

--- linknamu/src/main/java/com/kakao/linknamu/bookmark/service/BookmarkReadService.java
+++ linknamu/src/main/java/com/kakao/linknamu/bookmark/service/BookmarkReadService.java
@@ -1,13 +1,5 @@
 package com.kakao.linknamu.bookmark.service;
 
-import java.util.List;
-
-import com.kakao.linknamu.bookmark.dto.BookmarkRequestDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.kakao.linknamu.bookmark.BookmarkExceptionStatus;
 import com.kakao.linknamu.bookmark.dto.BookmarkResponseDto;
 import com.kakao.linknamu.bookmark.dto.BookmarkUserQueryDto;
@@ -18,8 +10,13 @@ import com.kakao.linknamu.core.exception.Exception403;
 import com.kakao.linknamu.core.exception.Exception404;
 import com.kakao.linknamu.tag.entity.Tag;
 import com.kakao.linknamu.user.entity.User;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -56,7 +53,7 @@ public class BookmarkReadService {
 		return BookmarkResponseDto.BookmarkGetResponseDto.of(bookmark, tagList);
 	}
 
-    public boolean existByBookmarkLinkAndCategoryId(String bookmarkLink, Long categoryId) {
-        return bookmarkJPARepository.findByCategoryIdAndBookmarkLink(categoryId, bookmarkLink).isPresent();
-    }
+	public boolean existByBookmarkLinkAndCategoryId(String bookmarkLink, Long categoryId) {
+		return bookmarkJpaRepository.findByCategoryIdAndBookmarkLink(categoryId, bookmarkLink).isPresent();
+	}
 }
