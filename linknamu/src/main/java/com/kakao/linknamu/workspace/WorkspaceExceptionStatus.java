6739d28ccbbc181085b8db8e0cff742d3460360e
ikyeong <grindabff@pusan.ac.kr>
2023-11-04 00:13:57 +0900

    #154 Refactor: workspace 에러코드 추가

--- linknamu/src/main/java/com/kakao/linknamu/workspace/WorkspaceExceptionStatus.java
+++ linknamu/src/main/java/com/kakao/linknamu/workspace/WorkspaceExceptionStatus.java
@@ -8,10 +8,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum WorkspaceExceptionStatus implements BaseExceptionStatus {
-	WORKSPACE_NOT_FOUND("존재하지 않는 워크스페이스 입니다.", 404),
-	WORKSPACE_FORBIDDEN("접근 권한이 없는 사용자 입니다.", 403),
-	WORKSPACE_DUPLICATED("같은 이름의 워크스페이스가 존재합니다.", 400);
+	WORKSPACE_DUPLICATED("같은 이름의 워크스페이스가 존재합니다.", 400, 64000),
+	WORKSPACE_FORBIDDEN("접근 권한이 없는 사용자 입니다.", 403, 64030),
+	WORKSPACE_NOT_FOUND("존재하지 않는 워크스페이스 입니다.", 404, 64040);
 
 	private final String message;
 	private final int status;
+	private final int errorCode;
 }
