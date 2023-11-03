dd12b1ad03a058b448a0603c2fa7399194055da3
ikyeong <grindabff@pusan.ac.kr>
2023-11-04 00:00:17 +0900

    #154 Refactor: tag, bookmarkTag 에러코드 추가

--- linknamu/src/main/java/com/kakao/linknamu/bookmarkTag/BookmarkTagExceptionStatus.java
+++ linknamu/src/main/java/com/kakao/linknamu/bookmarkTag/BookmarkTagExceptionStatus.java
@@ -7,12 +7,14 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum BookmarkTagExceptionStatus implements BaseExceptionStatus {
-	BOOKMARK_TAG_DUPLICATE("데이터가 이미 존재합니다.", 400),
-	BOOKMARK_TAG_FORBIDDEN("권한이 없는 접근입니다.", 403),
-	BOOKMARK_TAG_NOT_FOUND("대상이 존재하지 않습니다.", 404);
+	BOOKMARK_TAG_DUPLICATE("데이터가 이미 존재합니다.", 400, 54000),
+	BOOKMARK_TAG_FORBIDDEN("권한이 없는 접근입니다.", 403, 54030),
+	BOOKMARK_TAG_NOT_FOUND("대상이 존재하지 않습니다.", 404, 54040);
 
 	@Getter
 	private final String message;
 	@Getter
 	private final int status;
+	@Getter
+	private final int errorCode;
 }
